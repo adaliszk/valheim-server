7ff77cb9c266ae21290fd43bb22de3f39b2ebc23
Ádám Liszkai <adaliszk@gmail.com>
2021-03-08 10:06:00 +0100

    Add Severity prefixes

--- tools/vhpretty/source/vhpretty.py
+++ tools/vhpretty/source/vhpretty.py
@@ -5,35 +5,75 @@ from typing import Union
 
 class VhPretty:
 
-    @staticmethod
-    def parse(line: str) -> Union[str, None]:
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    def parse(self, line: str) -> Union[str, None]:
+        line = self.remove_noise(line)
+        if line is not None:
+            line = self.prefix_severity(line)
+        return line
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    def remove_noise(self, line: str) -> Union[str, None]:
 
         # ignore empty lines
-        if re.match(r'^\s*$', line):
+        if re.search(r'^\s*$', line):
             return None
 
         # ignore Debug lines with (Filename: ...)
-        if re.match(r'^\(Filename:', line):
+        if re.search(r'^\(Filename:', line):
             return None
+
+        # trim multiples spaces into one
+        multiple_spaces = re.compile(r'\s+')
+        if re.search(multiple_spaces, line):
+            line = re.sub(multiple_spaces, r' ', line)
 
         # remove DATETIME prefix as this is added outside of the STDOUT
         date_prefix = re.compile(r'^\d+/\d+/\d+ \d+:\d+:\d+:\s*', re.IGNORECASE)
-        if re.match(date_prefix, line):
+        if re.search(date_prefix, line):
             line = re.sub(date_prefix, r'', line)
 
         # remove Noisy prefixes
         noisy_prefix = re.compile(r'^(\[Subsystems]|-)\s*', re.IGNORECASE)
-        if re.match(noisy_prefix, line):
+        if re.search(noisy_prefix, line):
             line = re.sub(noisy_prefix, r'', line)
 
+        # remove WHITESPACE and format the line to look nice
+        line = self.ucfirst(line.strip())
         return line
 
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    def prefix_severity(self, line: str) -> Union[str, None]:
+
+        prefix = "I"
+
+        debug_triggers = re.compile(r'^(Base:|Redirecting to)', re.IGNORECASE)
+        if re.search(debug_triggers, line):
+            prefix = "D"
+
+        warning_triggers = re.compile(r'(Warning|Failed|Missing|Fallback)', re.IGNORECASE)
+        if re.search(warning_triggers, line):
+            prefix = "W"
+
+        error_triggers = re.compile(r'Error', re.IGNORECASE)
+        if re.search(error_triggers, line):
+            prefix = "E"
+
+        return prefix + "> " + line
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     @staticmethod
     def print(message: Union[str, None]) -> None:
-
         # only send something to the console if there is something
         if message is not None:
             print(message)
             sys.stdout.flush()
-
         pass
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    @staticmethod
+    def ucfirst(text: str):
+        if text is None or len(text) < 2:
+            return ""
+
+        return text[0].upper() + text[1:]
--- tools/vhpretty/source/vhpretty_test.py
+++ tools/vhpretty/source/vhpretty_test.py
@@ -9,32 +9,59 @@ from vhpretty import VhPretty
 class VhPrettyTest(TestCase):
 
     @parameterized.expand([
-        ("none", None, ""),
-        ("empty string", "", "\n"),
-        ("space", " ", " \n"),
+        ("nothing when None is passed", None, ""),
+        ("works with empty string", "", "\n"),
+        ("works with a whitespace", " ", " \n"),
     ])
     @mock.patch('sys.stdout', new_callable=io.StringIO)
-    def test_printing(self, _, message: str, expected, mock_stdout):
+    def test_that_printing(self, _, message: str, expected, mock_stdout):
         VhPretty.print(message)
         self.assertEqual(expected, mock_stdout.getvalue())
 
     @parameterized.expand([
-        ("empty line", "", None),
-        ("space", " ", None),
-        ("multiple spaces", "  ", None),
-
-        ("line goes through", "Lorem ipsum dolor sit amet", "Lorem ipsum dolor sit amet"),
-
-        ("filename debug line unix", "(Filename: consectetur/adipiscing/elit)", None),
-        ("filename debug line windows", "(Filename: consectetur\\adipiscing\\elit)", None),
-
-        ("removes date prefix iso", "2021/03/07 09:15:30: Etiam elementum et est nec ultrices", "Etiam elementum et est nec ultrices"),
-        ("removes date prefix freedom", "03/07/2021 09:15:30: Praesent a ligula vestibulum", "Praesent a ligula vestibulum"),
-        ("removes date prefix inverse", "07/03/2021 09:15:30: Fringilla diam eu, sagittis enim", "Fringilla diam eu, sagittis enim"),
-
-        ("removes [Subsystems] prefix", "[Subsystems] Nam sollicitudin vehicula tincidunt", "Nam sollicitudin vehicula tincidunt"),
-        ("removes dash from start", "- Nulla sed consectetur nunc", "Nulla sed consectetur nunc"),
-        ("keeps dash in the text", "Donec ut - velit com-modo", "Donec ut - velit com-modo"),
+        ("does not break on None", None, ""),
+        ("does not break on empty string", "", ""),
+        ("converts lowercase word", "dolor", "Dolor"),
+        ("does nothing on ucfirst word", "Lorem", "Lorem"),
+        ("does nothing on a sentence", "Lorem ipsum dolor sit amet.", "Lorem ipsum dolor sit amet."),
+        ("converts first word if starts with lowercase", "praesent a ligula vestibulum.", "Praesent a ligula vestibulum."),
     ])
-    def test_parsing(self, _, line: str, expected):
-        self.assertEqual(expected, VhPretty.parse(line))
+    def test_ucfirst_helper_that(self, _, text: str, expected):
+        self.assertEqual(expected, VhPretty.ucfirst(text))
+
+    @parameterized.expand([
+        ("removing empty line", "", None),
+        ("removing whitespace line", " ", None),
+        ("removing whitespace line no matter the length (2)", "  ", None),
+        ("removing whitespace line no matter the length (5)", "     ", None),
+        ("removing whitespace line no matter the length (12)", "            ", None),
+
+        ("replaces multiple spaces with one", "Pellentesque  vitae   venenatis    justo", "I> Pellentesque vitae venenatis justo"),
+
+        ("normal line goes through", "Lorem ipsum dolor sit amet", "I> Lorem ipsum dolor sit amet"),
+
+        ("removes filename debug line unix", "(Filename: consectetur/adipiscing/elit)", None),
+        ("removes filename debug line windows", "(Filename: consectetur\\adipiscing\\elit)", None),
+
+        ("removes date prefix iso", "2021/03/07 09:15:30: Etiam elementum et est nec ultrices", "I> Etiam elementum et est nec ultrices"),
+        ("removes date prefix freedom", "03/07/2021 09:15:30: Praesent a ligula vestibulum", "I> Praesent a ligula vestibulum"),
+        ("removes date prefix inverse", "07/03/2021 09:15:30: Fringilla diam eu, sagittis enim", "I> Fringilla diam eu, sagittis enim"),
+
+        ("removes [Subsystems] prefix", "[Subsystems] Nam sollicitudin vehicula tincidunt", "I> Nam sollicitudin vehicula tincidunt"),
+        ("removes dash from start", "- Nulla sed consectetur nunc", "I> Nulla sed consectetur nunc"),
+        ("keeps dash in the text", "Donec ut - velit com-modo", "I> Donec ut - velit com-modo"),
+    ])
+    def test_that_parsing_cleans_the_output_by(self, _, line: str, expected):
+        pretty = VhPretty()
+        self.assertEqual(expected, pretty.parse(line))
+
+    @parameterized.expand([
+        ("adding I prefix for the lines", "Lorem ipsum dolor sit amet", "I> Lorem ipsum dolor sit amet"),
+        ("adding W prefix for lines with warning", "Warning: Nam sollicitudin", "W> Warning: Nam sollicitudin"),
+        ("adding W prefix for lines with failed", "Nam sollicitudin Failed consectetur nunc", "W> Nam sollicitudin Failed consectetur nunc"),
+        ("adding W prefix for lines with missing", "Praesent a ligula missing!", "W> Praesent a ligula missing!"),
+        ("adding E prefix for lines with error", "Error: Vehicula tincidunt", "E> Error: Vehicula tincidunt"),
+    ])
+    def test_that_parsing_formats_the_output_by(self, _, line: str, expected):
+        pretty = VhPretty()
+        self.assertEqual(expected, pretty.parse(line))
