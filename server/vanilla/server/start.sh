f4ad1cb3efdbd355b36aee157dd642b70a2f7000
Ádám Liszkai <adaliszk@gmail.com>
2021-03-08 16:41:06 +0100

    fix Add ENV variable for the Public argument

--- server/vanilla/server/start.sh
+++ server/vanilla/server/start.sh
@@ -4,11 +4,10 @@ TEMP_LD_LIBRARY_PATH="$LD_LIBRARY_PATH"
 
 export LD_LIBRARY_PATH="${SERVER_PATH}/linux64:$LD_LIBRARY_PATH"
 
-./valheim_server.x86_64 -nographics -batchmode \
+./valheim_server.x86_64 -nographics -port 2456 \
   -name "${SERVER_MOTD}" \
   -world "${SERVER_WORLD}" \
   -password "${SERVER_PASSWORD}" \
-  -port 2456 \
-  -public 1
+  -public "${SERVER_PUBLIC}"
 
 export LD_LIBRARY_PATH="$TEMP_LD_LIBRARY_PATH"
