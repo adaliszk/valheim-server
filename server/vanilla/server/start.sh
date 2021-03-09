38468c11aa95f199916dba899847d732957b552e
Ádám Liszkai <adaliszk@gmail.com>
2021-03-09 03:15:58 +0100

    add New style of the Dockerfile

--- server/vanilla/server/start.sh
+++ server/vanilla/server/start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-TEMP_LD_LIBRARY_PATH="$LD_LIBRARY_PATH"
+echo "Uncompressing Server Files in $(pwd)"
+tar -xzf "${SERVER_PATH}/valheim_server_Data.tar.gz"
 
+TEMP_LD_LIBRARY_PATH="$LD_LIBRARY_PATH"
 export LD_LIBRARY_PATH="${SERVER_PATH}/linux64:$LD_LIBRARY_PATH"
 
 CMD="${SERVER_PATH}/valheim_server.x86_64 -nographics"
--- server/vanilla/srv/entrypoint.sh
+++ server/vanilla/srv/entrypoint.sh
@@ -18,14 +18,12 @@ function run {
   if [ -f "${SCRIPT}" ];
     then
       log "Executing \"${1}\" script..."
-      bash -c "${SCRIPT}" "${*:2}" 2>&1
+      bash -c "${SCRIPT}" "${*:2}" 2>&1 | tee "${LOG_PATH}/server-raw.log" | format-output &
+      SERVER=$!
 
-#      bash -c "${SCRIPT}" "${*:2}" 2>&1 | tee "${LOG_PATH}/server-raw.log" | format-output &
-#      SERVER=$!
-#
-#      tail -f "${LOG_PATH}/output.log" 2> /dev/null &
-#      wait "$SERVER"
-#      kill -TERM $! 2>/dev/null
+      tail -f "${LOG_PATH}/output.log" 2> /dev/null &
+      wait "$SERVER"
+      kill -TERM $! 2>/dev/null
     fi
 }
 
@@ -48,9 +46,5 @@ for LOG_FILE in {server-raw,server,output,error,backup,health,exit}
 do
   touch "${LOG_PATH}/${LOG_FILE}.log"
 done
-
-echo "Uncompressing Server Files in $(pwd)"
-tar -xzf "${SERVER_PATH}/valheim_server_Data.tar.gz"
-ls -la
 
 run "${CMD}" "${*:2}"
