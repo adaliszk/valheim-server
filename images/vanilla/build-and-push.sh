6e6858a8a3a04b1647ffdaec581948d3a7e84d70
Pierre Yager <pierre@levosgien.net>
2022-10-29 12:23:43 +0200

    Added a naïve build-and-push script for vanilla image

---
+++ images/vanilla/build-and-push.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+image=${DOCKER_USER:-adaliszk}/valheim-server
+
+echo "Building $image..."
+docker build . -t $image
+
+for tag in $(cat Dockertag); do
+  echo "Tagging $image:$tag"
+  docker tag $image $image:$tag
+done
+
+for tag in $(cat Dockertag); do
+  echo "Pushing $image:$tag"
+  docker push $image:$tag
+done
