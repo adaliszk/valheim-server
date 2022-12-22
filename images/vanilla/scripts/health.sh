c9908919c066bf408f476184caf97ecf1dabe334
Pierre Yager <pierre@levosgien.net>
2022-12-22 09:10:14 +0100

    fix(healthcheck): use -z flag for `nc` to avoid getting it stuck (#52)

--- images/vanilla/scripts/health.sh
+++ images/vanilla/scripts/health.sh
@@ -45,7 +45,7 @@ function check-port() {
   LAST_STATUS_FILE="${STATUS_PATH}/Port-${1}"
   LAST_PORT_STATUS="$(cat "${LAST_STATUS_FILE}" 2> /dev/null || echo 999)"
 
-  nc -u 0.0.0.0 "${1}"
+  nc -uz 0.0.0.0 "${1}"
   PORT_STATUS=$?
 
   log-debug-health "Port ${1} is $(status-to-word "${PORT_STATUS}")"
